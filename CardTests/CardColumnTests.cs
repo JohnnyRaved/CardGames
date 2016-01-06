183531b8c152415e4815333b0ab940e6049ed6d5
John Varney <jdvarney@msn.com>
2016-01-05 19:29:51 -0500

    Added CardColumn

---
+++ CardTests/CardColumnTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PlayingCards;
+
+namespace CardTests
+{
+    [TestClass]
+    public class CardColumnTests
+    {
+        [TestMethod]
+        public void NewCardColumnNotNull()
+        {
+            // Arrange
+            var gameDeck = new Deck();
+
+            // Act
+            var gameColumn1 = new CardColumn(gameDeck, 10);
+
+            // Assert
+            Assert.IsNotNull(gameColumn1);
+        }
+
+        [TestMethod]
+        public void NewCardColumnWith10Cards()
+        {
+            // Arrange
+            var gameDeck = new Deck();
+
+            // Act
+            var gameColumn1 = new CardColumn(gameDeck, 10);
+
+            // Assert
+            Assert.AreEqual(gameColumn1.Column.Count, 10);
+        }
+
+        [TestMethod]
+        public void GetCardsUpFromColumnWithNone_NotNull()
+        {
+            // Arrange
+            var gameDeck = new Deck();
+            var gameColumn = new CardColumn(gameDeck, 10);
+
+            // Act
+            List<int> cardsUpList;
+            cardsUpList = gameColumn.GetVisibleCards();
+
+            // Assert
+            Assert.IsNotNull(cardsUpList);
+        }
+
+        [TestMethod]
+        public void GetCardsUpFromColumnWithOne()
+        {
+            // Arrange
+            var gameDeck = new Deck();
+            var gameColumn = new CardColumn(gameDeck, 10);
+            gameColumn.TurnCardUp(gameColumn.Column[5]);
+
+            // Act
+            List<int> cardsUpList;
+            cardsUpList = gameColumn.GetVisibleCards();
+
+            // Assert
+            Assert.AreEqual(cardsUpList.Count, 1);
+        }
+
+        [TestMethod]
+        public void GetCardsUpWithUpperLimit()
+        {
+            // Arrange
+            var gameDeck = new Deck();
+            var gameColumn = new CardColumn(gameDeck, 10);
+            for (int i = 0; i < 10; i++)
+            {
+                gameColumn.TurnCardUp(gameColumn.Column[i]);
+            }
+
+            // Act
+            List<int> cardsUpList;
+            cardsUpList = gameColumn.GetVisibleCards();
+
+            // Assert
+            Assert.AreEqual(cardsUpList.Count, 10);
+
+        }
+
+    }
+}
---
+++ Playing Card Library/CardColumn.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace PlayingCards
+{
+    /// <summary>
+    /// Represents a column of cards
+    /// </summary>
+    public class CardColumn
+    {
+        public Dictionary<int, Card> Column = new Dictionary<int, Card>();
+        private List<int> visibleCards = new List<int>();
+        public Deck GameDeck { get; private set; }
+
+        /// <summary>
+        /// Given a deck of cards and initial card count, creates a CardColumn
+        /// </summary>
+        /// <param name="deck">Deck of cards</param>
+        /// <param name="initialCardCount">Number of cards to put in column</param>
+        public CardColumn(Deck deck, int initialCardCount)
+        {
+            if (deck.Cards.Count < initialCardCount) throw new ArgumentOutOfRangeException();
+
+            GameDeck = deck;
+            Column = new Dictionary<int, Card>();
+            Column = deck.Deal(Column, initialCardCount);
+        }
+        
+        /// <summary>
+        /// Returns a key list of cards visible to the user
+        /// </summary>
+        /// <returns>Null</returns>
+        public List<int> GetVisibleCards()
+        {
+            List<int> visibleCards = new List<int>();
+
+            foreach (KeyValuePair<int, Card> kvp in Column)
+            {
+                if (kvp.Value.IsFaceDown == false) visibleCards.Add(kvp.Key);
+            }
+            return visibleCards;
+        }
+
+        /// <summary>
+        /// Makes a card 'visible'
+        /// </summary>
+        /// <param name="card"></param>
+        public void TurnCardUp(Card card)
+        {
+            card.IsFaceDown = false;
+        }
+
+        /// <summary>
+        /// Marks a card as 'invisible'
+        /// </summary>
+        /// <param name="card"></param>
+        public void TurnCardDown(Card card)
+        {
+            card.IsFaceDown = true;
+        }
+    }
+}
